0c6d7aba889114c8a56938326545eca8fcf18922
James-GUIRG <46895080+James-GUIRG@users.noreply.github.com>
2019-02-01 17:06:56 +0100

    Add files via upload
    Ajout de la fonction "changement_angle" + le jeux de test associé. Correction en ajoutant les parenthèses aux commandes print

--- robot.py
+++ robot.py
@@ -11,10 +11,32 @@ class Robot(object):
 		return self.x, self.y
 
 
+def changement_angle(robot, angle):
+    if angle > 0:
+        angle = angle%360
+    else :
+        angle = angle%(-360)
+        
+    robot.angle += angle
+
 #jeu de test:
 a=Robot(0,0,90)
-print a.get_position()==(0,0)
+print (a.get_position()==(0,0))
 a.x,a.y=a.x+1,a.y+1
-print a.get_position()==(1,1)
-print a.angle==90
+print (a.get_position()==(1,1))
+print (a.angle==90)
 
+#--------------------------------------------------
+#jeu de test (fct changement angle):
+changement_angle(a, 30)#ajout de 30°
+print(a.angle==90)#False verifie si modification resultat correct
+print(a.angle==120)#True verifie si resultat correct
+
+changement_angle(a, -30)#-30°
+print(a.angle==90)#True resultat correct
+changement_angle(a, 390)#+390 => +30 
+print(a.angle==120)#True resultat correct
+changement_angle(a, -390)#-390° => -30°
+print(a.angle==90)#True resultat correct
+changement_angle(a, 0)#angle nul
+print(a.angle==90)#True resultat correct
